1a380e1a77905bf598bde6e9e23abf44e8801cbc
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-03-05 12:57:05 +0100

    Update midi.js

--- midi.js
+++ midi.js
@@ -43,7 +43,7 @@ document.addEventListener('keydown', function(e) {
       setTimeout(function(){
         document.getElementById("K" + Keys[e.key]).style.backgroundColor = "";
       }, 1000);
-      audioCtx.resume()
+      audioCtx.resume();
       Play(Keys[e.key]);
     } else if (audioCtx.state === "running") {
       document.getElementById("K" + Keys[e.key]).style.backgroundColor = "red";
@@ -61,9 +61,29 @@ document.addEventListener('keyup', function(e) {
   }
 });
 
+navigator.requestMIDIAccess().then((access) => {
+  const inputs = access.inputs.values();
+  const outputs = access.outputs.values();
+  console.log(inputs + " " + outputs);
+
+  inputs.forEach((input) => {
+    console.log(input.name);
+    input.onmidimessage = (message) => {
+      const [command, key, velocity] = message.data;
+
+      if (command === 144) {
+      	audioCtx.resume();
+      	Play(key);
+      } else if(command === 128) {
+        audioCtx.suspend();
+      }
+    };
+  })
+});
+
 function Play(m) {
-  oscillatorNode.type = "triangle"; // types: sine, square, sawtooth, triangle.
-  oscillatorNode.frequency.setTargetAtTime(Math.pow(2, (m-69)/12)*250, audioCtx.currentTime, 0); // frequency Hz
+  oscillatorNode.type = "square"; // types: sine, square, sawtooth, triangle.
+  oscillatorNode.frequency.setTargetAtTime(Math.pow(2, (m-69)/12)*100, audioCtx.currentTime, 0); // frequency Hz
   oscillatorNode.connect(audioCtx.destination);
   oscillatorNode.start();
 }
